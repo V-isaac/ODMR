b2f78e2aeef77198ebeedf113186ad3029b59ba9
V-isaac <79458239+V-isaac@users.noreply.github.com>
2025-01-28 10:18:57 +0300

    [feat] added ADA2200 pins on HSPI

--- ODMR_launch/ODMR_config.h
+++ ODMR_launch/ODMR_config.h
@@ -17,33 +17,38 @@
 
 #ifdef ALTERNATE_PINS
 
-// ADA2200
-#define VSPI_MISO 14 // RDY pin
-#define VSPI_MOSI 12 // DIN pin
-#define VSPI_SCLK 23 // Serial Clock
-#define VSPI_SS   22 // Chip Select
+  // AD7192
+  #define VSPI_MISO 14 // RDY pin Srial Data Out. LOW when data is ready to be read
+  #define VSPI_MOSI 12 // DIN pin to the Input Shift Register
+  #define VSPI_SCLK 23 // Serial Clock
+  #define VSPI_SS   22 // Chip Select
 
-#define HSPI_MISO 26 
-#define HSPI_MOSI 27
-#define HSPI_SCLK 25
-#define HSPI_SS   32
+  // ADA2200
+  #define HSPI_MISO 26 //	Doesn't seem to go into ESP32
+  #define HSPI_MOSI 27 // SDIO/SDA
+  #define HSPI_SCLK 21 // Serial Clock
+  #define HSPI_SS   25 // invCS
 #else
-#define VSPI_MISO MISO
-#define VSPI_MOSI MOSI
-#define VSPI_SCLK SCK
-#define VSPI_SS   SS
+  #define VSPI_MISO MISO
+  #define VSPI_MOSI MOSI
+  #define VSPI_SCLK SCK
+  #define VSPI_SS   SS
 
-#define HSPI_MISO 12
-#define HSPI_MOSI 13
-#define HSPI_SCLK 14
-#define HSPI_SS   15
+  #define HSPI_MISO 12
+  #define HSPI_MOSI 13
+  #define HSPI_SCLK 14
+  #define HSPI_SS   15
 #endif
 
 #if !defined(CONFIG_IDF_TARGET_ESP32)
 #define VSPI FSPI
 #endif
 
-// Example message
+// Example messages
+#define CONTINIOUS_READ  0b01011100
+#define ONE_TIME_READ    0b01011000 //send while RDY pin is low
+
+
 // #define BINARY 0b01010101
 
 // --------------------------------------------- //
