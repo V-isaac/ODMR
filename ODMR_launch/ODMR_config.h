0294c0348d5e17c562d9808b6e3298942b9f78d6
V-isaac <79458239+V-isaac@users.noreply.github.com>
2025-01-29 10:54:22 +0300

    [refactor/add] Deleted unnecessary definitions. Added ADA2200 pins

--- ODMR_launch/ODMR_config.h
+++ ODMR_launch/ODMR_config.h
@@ -13,40 +13,27 @@
 // --------------------------------------------- //
 
 // SPI definitions
-#define ALTERNATE_PINS // Alternative pins if defined
-
-#ifdef ALTERNATE_PINS
-
-  // AD7192
-  #define VSPI_MISO 14 // RDY pin Srial Data Out. LOW when data is ready to be read
-  #define VSPI_MOSI 12 // DIN pin to the Input Shift Register
-  #define VSPI_SCLK 23 // Serial Clock
-  #define VSPI_SS   22 // Chip Select
-
-  // ADA2200
-  #define HSPI_MISO 26 //	Doesn't seem to go into ESP32
-  #define HSPI_MOSI 27 // SDIO/SDA
-  #define HSPI_SCLK 21 // Serial Clock
-  #define HSPI_SS   25 // invCS
-#else
-  #define VSPI_MISO MISO
-  #define VSPI_MOSI MOSI
-  #define VSPI_SCLK SCK
-  #define VSPI_SS   SS
-
-  #define HSPI_MISO 12
-  #define HSPI_MOSI 13
-  #define HSPI_SCLK 14
-  #define HSPI_SS   15
-#endif
+// AD7192
+#define VSPI_MISO 14 			 // RDY pin Srial Data Out. LOW when data is ready to be read
+#define VSPI_MOSI 12			 // DIN pin to the Input Shift Register
+#define VSPI_SCLK 23			 // Serial Clock
+#define VSPI_SS   22			 // Chip Select
+#define ADCORDER	MSBFIRST // What it says
+													 
+// ADA2200
+#define HSPI_MISO 26			 //	Doesn't seem to go into ESP32
+#define HSPI_MOSI 27			 // SDIO/SDA - bidirectional in/out
+#define HSPI_SCLK 21			 // Serial Clock
+#define HSPI_SS   25			 // invCS
+#define FILORDER	MSBFIST	 // What it says
 
 #if !defined(CONFIG_IDF_TARGET_ESP32)
 #define VSPI FSPI
 #endif
 
 // Example messages
-#define CONTINIOUS_READ  0b01011100
-#define ONE_TIME_READ    0b01011000 //send while RDY pin is low
+// #define CONTINIOUS_READ  0b01011100
+// #define ONE_TIME_READ    0b01011000 //send while RDY pin is low
 
 
 // #define BINARY 0b01010101
