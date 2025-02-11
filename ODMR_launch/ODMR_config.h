15d6952af27d196844fef9cb08428f0d36c00e6f
V-isaac <79458239+V-isaac@users.noreply.github.com>
2025-02-11 08:45:32 +0300

    feat: AMP config

--- ODMR_launch/ODMR_config.h
+++ ODMR_launch/ODMR_config.h
@@ -10,32 +10,24 @@
 #define MAX_DUTY_CYCLE (int)(pow(2, PWM_RESOLUTION) - 1)
 #define LED_OUTPUT_PIN 18
 
-// --------------------------------------------- //
-
 // SPI definitions
-// AD7192
-#define VSPI_MISO 14 			 // RDY pin Srial Data Out. LOW when data is ready to be read
-#define VSPI_MOSI 12			 // DIN pin to the Input Shift Register
-#define VSPI_SCLK 23			 // Serial Clock
-#define VSPI_SS   22			 // Chip Select
-#define ADCORDER	MSBFIRST // What it says
-													 
+// AD7192 the ADC
+#define VSPI_MISO 12 			 	// RDY pin Srial Data Out. LOW when data is ready to be read
+#define VSPI_MOSI 14			 	// DIN pin to the Input Shift Register
+#define VSPI_SCLK 23			 	// Serial Clock
+#define VSPI_SS   22			 	// Chip Select
+#define ADCORDER	MSBFIRST 	// What it says
+#define ADC_MODE	SPI_MODE3	// single definition of SPI mode
+
 // ADA2200
-#define HSPI_MISO 26			 //	Doesn't seem to go into ESP32
-#define HSPI_MOSI 27			 // SDIO/SDA - bidirectional in/out
-#define HSPI_SCLK 21			 // Serial Clock
-#define HSPI_SS   25			 // invCS
-#define FILORDER	MSBFIST	 // What it says
+#define HSPI_MISO 26			 	//	Doesn't seem to go into ESP32
+#define HSPI_MOSI 27			 	// SDIO/SDA - bidirectional in/out
+#define HSPI_SCLK 21			 	// Serial Clock
+#define HSPI_SS   25			 	// invCS
+#define FILORDER	MSBFIRST 	// What it says
+#define AMP_MODE	SPI_MODE3	// single definition of spi mode
+#define CLKIN							 	// need to look into clock pin
 
 #if !defined(CONFIG_IDF_TARGET_ESP32)
 #define VSPI FSPI
 #endif
-
-// Example messages
-// #define CONTINIOUS_READ  0b01011100
-// #define ONE_TIME_READ    0b01011000 //send while RDY pin is low
-
-
-// #define BINARY 0b01010101
-
-// --------------------------------------------- //
