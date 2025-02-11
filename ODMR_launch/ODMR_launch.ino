60fc34408d38c62634fe2f0481cc768a753c6242
V-isaac <79458239+V-isaac@users.noreply.github.com>
2025-02-11 08:43:38 +0300

    feat: ADC full config with continious read

--- ODMR_launch/ODMR_launch.ino
+++ ODMR_launch/ODMR_launch.ino
@@ -2,93 +2,113 @@
 
 #include "ODMR_config.h"
 #include "ADC_config.h"
+#include "AMP_config.h"
 
 // PWM setup
 int freq;
-uint8_t binary, b_temp, b_in;
-byte temp; // 8 bits for temp
+int temp;
+// for messages: 0xFF == 0b11111111
 
-static const int spiClk = 10000;  // 10 kHz spiClock - bare minimum for AD7192
+/*
+Chip		min	max units
+ADA2200			20 	MHz
+AD7192	10  10k	kHz
+*/
+static const int spiClk = 200000;
+//static const int spiClk = 49000000; 
+
 //uninitialized pointers to SPI objects
 SPIClass *vspi = NULL;
-SPIClass *hspi = NULL;
+// SPIClass *hspi = NULL;
 
 void setup() {
   Serial.begin(115200);
+  Serial.print("entered setup \n\r");
 
-  ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
-  ledcAttachPin(LED_OUTPUT_PIN, PWM_CHANNEL);
-  ledcWrite(PWM_CHANNEL, MAX_DUTY_CYCLE/2);
+  // ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RESOLUTION);
+  // ledcAttachPin(CLKIN, PWM_CHANNEL);
+  // ledcWrite(PWM_CHANNEL, MAX_DUTY_CYCLE/2);
 
-  // Using only 2 available SPIs
+  // initialising SPI protocols, using only 2 available on ESP32
   vspi = new SPIClass(VSPI);
-  hspi = new SPIClass(HSPI);
-  // Defining SPIs pins
-  // pins of our choice
-  vspi->begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);  //23, MISO, MOSI, SS
-  
-  #ifndef ALTERNATE_PINS
-    // default SCLK = 14, MISO = 12, MOSI = 13, SS = 15
-    hspi->begin();
-  #else
-    // pins of our choice
-    hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_SS);  //SCLK, MISO, MOSI, SS
-  #endif
+  // hspi = new SPIClass(HSPI);
 
-  // Setting up Chip Select pins as Arduino IDE doesn't do that by default
-  pinMode(vspi->pinSS(), OUTPUT);  //VSPI SS
-  pinMode(hspi->pinSS(), OUTPUT);  //HSPI SS
+  // Defining SPIs pins
+  vspi -> begin(VSPI_SCLK, VSPI_MISO, VSPI_MOSI, VSPI_SS);  //SC  MISO, MOSI, SS
+	// hspi -> begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_SS);  //SCLK, MISO, MOSI, SS
 	
-	// configuring ADC
-	vspi -> beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
+	// Explicitly setting pins as output/input
+  pinMode(vspi->pinSS(), OUTPUT);  
+	// Ensure CS is high at initialisation
+	digitalWrite(vspi -> pinSS(), HIGH);
+	// digitalWrite(hspi -> pinSS(), HIGH);
+  ADCresert();
+
+  vspi -> beginTransaction(SPISettings(spiClk, ADCORDER, ADC_MODE));
+	digitalWrite(vspi -> pinSS(), LOW);
+	vspi -> transfer(con_reg);
+  vspi -> transfer(conf1);
+  vspi -> transfer(conf2);
+  vspi -> transfer(conf3);
+  digitalWrite(vspi -> pinSS(), HIGH);
+  vspi -> endTransaction();
+	delay(1);
+
+	vspi -> beginTransaction(SPISettings(spiClk, ADCORDER, ADC_MODE));
 	digitalWrite(vspi -> pinSS(), LOW);
 	vspi -> transfer(mode_reg);
-	WAIT();
-	vspi -> transfer(mode1);
-	WAIT();
-	vspi -> transfer(mode2);
-	WAIT();
-	vspi -> transfer(mode3);
-	WAIT();
-
-	vspi -> transfer(comm_reg);
-	WAIT();
-	vspi -> transfer(conf1);
-	WAIT();
-	vspi -> transfer(conf2);
-	WAIT();
-	vspi -> transfer(conf3); 
-	WAIT();
-	digitalWrite(VSPI_MOSI, HIGH);
-	digitalWrite(vspi -> pinSS(), HIGH);
+  vspi -> transfer(mode1);
+  vspi -> transfer(mode2);
+  vspi -> transfer(mode3);
+  digitalWrite(vspi -> pinSS(), HIGH);
+  vspi -> endTransaction();
+	delay(1);
+	
+	// setting up continious read
+	vspi -> beginTransaction(SPISettings(spiClk, ADCORDER, ADC_MODE));
 	digitalWrite(vspi -> pinSS(), LOW);
-	vspi -> endTransaction();
+	vspi -> transfer(read_cont); 
 
-	attachInterrupt(digitalPinToInterrupt(VSPI_MISO), ADCread, FALLING);
+	// attachInterrupt(digitalPinToInterrupt(VSPI_MISO), ADCread, FALLING); // execute function on falling edge of MISO
+  mPrint(Serial, "Done setting up \n\r"); 
 }
 
 void loop() {
+  while(digitalRead(VSPI_MISO) != LOW){}
+  /* while (1){
+   if (Serial.available()){ temp = Serial.read(); break;} 
+  } */
+  out1 = vspi -> transfer(0x00);
+	out2 = vspi -> transfer(0x00);
+	out3 = vspi -> transfer(0x00);
+		//digitalWrite(vspi -> pinSS(), HIGH); 
+		//vspi -> endTransaction();
 
-  // Re-setup PWM on current channel with said frequency
-  // ledcSetup(PWM_CHANNEL, freq, PWM_RESOLUTION);
-  // ledcWrite(PWM_CHANNEL, MAX_DUTY_CYCLE/2);
-  // mPrint(Serial, "LEDC reconfigured to ", freq, "\n\r");
+  var = 0;
+  var = out1;
+  var = var << 8 + out2; 
+  var = var << 8 + out3; 
+	mPrint(Serial, var, "\n\r");
 }
 
-inline void WAIT(){
-	delay(5);
-}
 
+void ADCresert(){
+	vspi -> beginTransaction(SPISettings(spiClk, ADCORDER, ADC_MODE)); //  is high when inactive, sample on falling edge
+	digitalWrite(vspi -> pinSS(), LOW);
+	vspi -> transfer(0xFF); // 8 bits each
+	vspi -> transfer(0xFF); // 16
+	vspi -> transfer(0xFF); // 24
+	vspi -> transfer(0xFF); // 32
+	vspi -> transfer(0xFF); // 40
+	digitalWrite(vspi -> pinSS(), HIGH);
+	vspi -> endTransaction();
+
+	delay(1);
+}
 // execute on interrupt - we have data to read
 void ADCread(){
-		vspi -> beginTransaction(SPISettings(spiClk, MSBFIRST, SPI_MODE0));
-		digitalWrite(vspi -> pinSS(), LOW);  
-		vspi -> transfer(send_data);
-		out1 = vspi -> transfer(0xff);
-		out2 = vspi -> transfer(0xff);
-		out3 = vspi -> transfer(0xff);
-		digitalWrite(vspi -> pinSS(), HIGH); 
-		vspi -> endTransaction();
-
-		mPrint(Serial, "\n\r", out1, "\t", out2, "\t", out3, "\n\r");
+		//vspi -> beginTransaction(SPISettings(spiClk, ADCORDER, ADC_MODE));
+		//digitalWrite(vspi -> pinSS(), LOW);  
+		//vspi -> transfer(send_data);
+		
 }
